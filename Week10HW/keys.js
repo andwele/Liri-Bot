3607a2f4bb0a339d2d19265b57d0f2e66a2d76c0
Michael Hill <mike.hill.42@gmail.com>
2017-04-08 10:30:26 -0400

    Add files via upload

---
+++ Week10HW/keys.js
@@ -0,0 +1,18 @@
+
+console.log("this is loaded");
+
+
+exports.twitterKeys = {
+  consumer_key: "GSutd0hsoDQkp0eVXR8mCv5Jh",
+  consumer_secret: "KDHceggXs6FtMFvJ7cmiWmGoSNY62P7rueJwkliTTaHBKVdWRI",
+  access_token_key: "43803697-ZUQWQtMpyJTwA6CfHDQmmwBARIQF1nnUn20njbn1a",
+  access_token_secret: "B9mgfdCPb2nfTywBjf9KOUBJw5KYjk0UkCABr2ke9Wtg7",
+}
+
+exports.spotifyKeys = {
+
+}
+
+exports.omdbKeys = {
+
+}
